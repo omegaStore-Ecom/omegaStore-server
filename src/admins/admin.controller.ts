21648ebf27562f0e53bc734182f325b1478193b4
Redouane Bouabana <rredouane342@gmail.com>
2022-03-21 20:32:49 +0100

    move to logic controller to the service as it suppose to be

--- src/admins/admin.controller.ts
+++ src/admins/admin.controller.ts
@@ -1,9 +1,8 @@
 import { Controller, Get, Post, Body, Put, Param, Res } from '@nestjs/common';
-import { Admin, AdminDocument } from 'src/schemas/admin.schema';
+import { Admin } from 'src/schemas/admin.schema';
 import { AdminService } from './admin.service';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { AuthService } from 'src/auth/auth.service';
-import { LoginDTO } from './../auth/login.dto';
 
 @Controller('Admins')
 export class AdminController {
@@ -13,33 +12,13 @@ export class AdminController {
   ) {}
 
   @Post('register')
-  async create(@Body() admin: Admin) {
-    const newAdmin = await this.adminsService.create(admin);
-    const payload = {
-      email: admin.email,
-    };
-
-    const token = await this.authService.signPayload(payload);
-    return {
-      token,
-      admin: newAdmin,
-    };
+  async register(@Body() admin: Admin, @Res() res) {
+    return await this.adminsService.register(admin, res);
   }
 
   @Post('login')
   async login(@Body() admin: Admin, @Res() res) {
-    const { email, password } = admin;
-    const existAdmin = await this.adminsService.findOne(email);
-
-    // const payload = {
-    //   email: existAdmin.email,
-    // };
-
-    // const token = await this.authService.signPayload(payload);
-    return res.status(200).json({
-      // token,
-      admin: existAdmin,
-    });
+    return await this.adminsService.login(admin, res);
   }
 
   @Get()
@@ -47,10 +26,10 @@ export class AdminController {
     return this.adminsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') name: string) {
-    return this.adminsService.findOne(name);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') name: string) {
+  //   return this.adminsService.findOne(name);
+  // }
 
   @Put(':id')
   update(@Param('id') name: string, @Body() updateAdminDto: UpdateAdminDto) {
--- src/admins/admin.service.ts
+++ src/admins/admin.service.ts
@@ -2,22 +2,37 @@ import { Injectable, Post, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Admin } from 'src/schemas/admin.schema';
-import { AdminRegisterDto } from './dto/register-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class AdminService {
-  constructor(@InjectModel(Admin.name) private AdminModel: Model<Admin>) {}
+  constructor(
+    @InjectModel(Admin.name) private AdminModel: Model<Admin>,
+    private authService: AuthService,
+  ) {}
 
-  async create(adminRegisterDto: AdminRegisterDto) {
-    const { email } = adminRegisterDto;
-    const admin = await this.AdminModel.findOne({ email });
-    if (admin) {
-      throw new Error('Admin already exists');
+  // admin register
+
+  async register(admin, res) {
+    const { email } = admin;
+    const existingAdmin = await this.AdminModel.findOne({ email });
+    if (existingAdmin) {
+      res.status(400).json({
+        message: 'Admin already exists',
+      });
     }
-    const newAdmin = new this.AdminModel(adminRegisterDto);
-    await newAdmin.save();
-    return this.sanitizeAdmin(newAdmin);
+    const newAdmin = await this.AdminModel.create(admin);
+    const payload = {
+      email: admin.email,
+    };
+
+    const token = await this.authService.signPayload(payload);
+
+    return res.status(201).json({
+      token,
+      admin: this.sanitizeAdmin(newAdmin),
+    });
   }
 
   sanitizeAdmin(admin: Admin) {
@@ -26,12 +41,23 @@ export class AdminService {
     return sanitized;
   }
 
+  // admin login
+
+  async login(admin, res) {
+    const { email, password } = admin;
+    const existAdmin = await this.AdminModel.findOne({ email });
+    const payload = {
+      email: existAdmin.email,
+    };
+
+    const token = await this.authService.signPayload(payload);
+    return res.status(200).json({
+      token,
+      admin: existAdmin,
+    });
+  }
   async findAll() {
     return this.AdminModel.find();
-  }
-
-  async findOne(email: string) {
-    return this.AdminModel.findOne({ email });
   }
 
   async update(name: string, updateAdminDto: UpdateAdminDto) {
