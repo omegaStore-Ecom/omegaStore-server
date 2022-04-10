e369ba95e2713aabe4c264cb3b3a513355de42b7
Redouane Bouabana <rredouane342@gmail.com>
2022-04-10 21:50:12 +0000

    Delete dist directory

--- dist/types/collection.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Collection extends Document {
-    collectionName: string;
-    collectionDescription: string;
-    collectionStatus: number;
-    collectionImage: string;
-    collectionOwner: string;
-}
